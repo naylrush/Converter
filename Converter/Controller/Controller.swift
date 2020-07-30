4b5137035847f6ef5ecfd36b8eff76e83a73d3eb
Alexey Davletshin <naylrush@gmail.com>
2020-07-30 21:02:06 +0300

    Initial xcode commit

---
+++ Converter/Controller/Controller.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Converter
+//
+//  Created by Alexey Davletshin on 30.07.2020.
+//  Copyright © 2020 Alexey Davletshin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+}
---
+++ ConverterTests/ConverterTests.swift
@@ -0,0 +1,26 @@
+//
+//  ConverterTests.swift
+//  ConverterTests
+//
+//  Created by Alexey Davletshin on 30.07.2020.
+//  Copyright © 2020 Alexey Davletshin. All rights reserved.
+//
+
+import XCTest
+
+class ConverterTests: XCTestCase {
+  override func setUpWithError() throws {
+      // Put setup code here. This method is called before the invocation of each test method in the class.
+  }
+
+  override func tearDownWithError() throws {
+      // Put teardown code here. This method is called after the invocation of each test method in the class.
+  }
+
+  func testPerformanceExample() throws {
+      // This is an example of a performance test case.
+      measure {
+          // Put the code you want to measure the time of here.
+      }
+  }
+}
